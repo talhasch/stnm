d510f63ca8650f2615660f760600d75ca59c0608
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-12-05 10:24:57 +0300

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ assert sys.version_info[0] == 3 and sys.version_info[1] >= 5, "Requires Python 3
 
 setup(
     name="stnm",
-    version="0.1.1",
+    version="0.1.2",
     description="Stacks blockchain node process manager",
     long_description="Stacks blockchain node process manager. See https://github.com/talhasch/stnm for mode details.",
     packages=find_packages(exclude=("venv",)),
