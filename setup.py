7aa8e4c208b677a13796fc575905306cb378c253
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-12-07 22:15:06 +0300

    setup file update

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import sys
+from glob import glob
 
 from setuptools import find_packages
 from setuptools import setup
@@ -7,13 +8,18 @@ assert sys.version_info[0] == 3 and sys.version_info[1] >= 5, "Requires Python 3
 
 setup(
     name="stnm",
-    version="0.2.0",
+    version="0.2.1",
     author="Talha Buğra Bulut",
     author_email="talhabugrabulut@gmail.com",
     url="https://github.com/talhasch/stnm",
     description="Stacks blockchain node process manager",
     long_description="Stacks blockchain node process manager. See https://github.com/talhasch/stnm for mode details.",
     packages=find_packages(exclude=("venv",)),
+    data_files=[
+        ('web_static', glob('stnm/web/static/**/**')),
+        ('web_templates', glob('stnm/web/templates/**/**')),
+        # etc...
+    ],
     install_requires=[
         "psutil==5.7.3",
         "toml==0.10.2",
