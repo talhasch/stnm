c7990d2cde90ed60ac00195160706634218edc1c
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-12-08 20:05:39 +0300

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ assert sys.version_info[0] == 3 and sys.version_info[1] >= 5, "Requires Python 3
 
 setup(
     name="stnm",
-    version="0.2.3",
+    version="0.3.0",
     author="Talha Buğra Bulut",
     author_email="talhabugrabulut@gmail.com",
     url="https://github.com/talhasch/stnm",
